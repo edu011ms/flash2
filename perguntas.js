db7ecf8d59337b7de719c0fa8033d102d37140cf
edu011ms <matos.souza.eduardo@escola.pr.gov.br>
2024-10-09 12:10:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Programação',
+    'Qual é o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é Júpiter.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'A capital da Austrália é Canberra.'
+)
+criaCartao(
+    'Programação',
+    'Quem escreveu "Dom Quixote"?',
+    '"Dom Quixote" foi escrito por Miguel de Cervantes.'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Qual é o principal ingrediente do guacamole?',
+    'O principal ingrediente do guacamole é o abacate.'
+)
+criaCartao(
+    'Programação',
+    'Qual animal é conhecido como o "rei da selva"?',
+    'O leão é conhecido como o "rei da selva".'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a fórmula química da água?',
+    'A fórmula química da água é H₂O.'
+)
+criaCartao(
+    'Programação',
+    'Qual é o continente mais populoso do mundo?',
+    'O continente mais populoso do mundo é a Ásia.'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Qual é o nome da moeda utilizada no Japão?',
+    'A moeda utilizada no Japão é o iene'
+)
+criaCartao(
+    'Programação',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o principal gás responsável pelo efeito estufa?',
+    'O principal gás responsável pelo efeito estufa é o dióxido de carbono (CO₂).'
+)
+criaCartao(
+    'Programação',
+    'Qual é a capital da França?',
+    'A capital da França é Paris.'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a linguagem de programação mais popular atualmente?',
+    'ma das linguagens de programação mais populares atualmente é o Python.'
+)
